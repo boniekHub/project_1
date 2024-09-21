e5f9065f9828503dc495f8e3288b7b1543b796ea
szymon_bonkowski <szymon.bonkowski09@gmail.com>
2024-09-21 12:46:40 +0200

    Zapis

--- testy.py
+++ testy.py
@@ -1,4 +1,10 @@
-ciag_znakow = input("Podaj dowolny ciąg znaków: ")
-pierwsza_litera = ciag_znakow[0]
-ostatnia_litera = ciag_znakow[-1]
-print(f"Pierwsza litera: {pierwsza_litera}, Ostatnia litera: {ostatnia_litera}")
+print("2-=-"*10)
+print(numbers)
+
+
+i = 0
+while i < 5:
+    inp = float(input())
+    numbers.append(inp)
+    print(numbers)
+    i += 1
